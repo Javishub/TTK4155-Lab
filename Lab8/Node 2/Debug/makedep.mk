7e205e9b5ccae622ee0ffcc035d374c194241bf0
Larsaern <144334717+Larsaern@users.noreply.github.com>
2023-11-07 19:07:51 +0100

    Add files via upload

---
+++ Lab8/Node 2/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+can_controller.c
+
+can_interrupt.c
+
+Device_Startup\startup_sam3xa.c
+
+Device_Startup\system_sam3xa.c
+
+main.c
+
+PID.c
+
+printf-stdarg.c
+
+time.c
+
+uart.c
+
---
+++ Lab8/Node 2/game.h
@@ -0,0 +1,13 @@
+/*
+ * game.h
+ *
+ * Created: 02.11.2023 17:13:55
+ *  Author: larskrud
+ */ 
+
+/*
+int gamescore(int score){
+	score++
+}SysTick
+
+TRNG*/
